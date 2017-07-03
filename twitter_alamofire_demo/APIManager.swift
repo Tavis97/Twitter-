12405245054b0507985661324b757b53db705de7
Tavis Thompson <tavis97@fb.com>
2017-07-03 14:21:17 -0700

    Added tokens to APIManager

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -15,8 +15,8 @@ import KeychainAccess
 class APIManager: SessionManager {
     
     // MARK: TODO: Add App Keys
-    static let consumerKey = "YOUR_KEY_HERE"
-    static let consumerSecret = "YOUR_SECRET_HERE"
+    static let consumerKey = "Jy0Spgp6HHS8boh7FuFO8jjO8"
+    static let consumerSecret = "7hky2PD54nBvBkJTF6zeJLArXkOH5bNpLDqRMH7XiL5RzKWdNS"
     
     static let requestTokenURL = "https://api.twitter.com/oauth/request_token"
     static let authorizeURL = "https://api.twitter.com/oauth/authorize"
